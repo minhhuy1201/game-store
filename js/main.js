721a770944746563c7729cc265af49a7f788c820
huykirito1201 <huyyhuqqho@gmail.com>
2023-09-30 23:05:36 +0700

    first

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+
+// VARIBLES
+const menuIcon = $(".menu-icon")
+const navBar = $(".menu")
+const bellIcon = $(".noti") // for notification
+
+// MENU ICON
+menuIcon.onclick = () => {
+    navBar.classList.toggle('active')
+    menuIcon.classList.toggle('move')
+    bellIcon.classList.remove("active")
+}
+
+// NOTIFICATION
+$("#bell-icon").onclick = () => {
+    bellIcon.classList.toggle("active")
+}
+
+// SWIPER
+var swiper = new Swiper(".trending_content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000, // 3s
+        disableOnInteraction: false,
+
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+        },
+    },
+});
